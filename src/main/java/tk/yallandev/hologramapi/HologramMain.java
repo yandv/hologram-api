8faae8942e4e5f311b2455c354245ecb1dc58847
TheNameOfDreams <Blackwindow2109>
2020-06-04 20:48:17 -0300

    HologramMain to test the functions

--- src/main/java/tk/yallandev/hologramapi/HologramMain.java
+++ src/main/java/tk/yallandev/hologramapi/HologramMain.java
@@ -1,8 +1,15 @@
 package tk.yallandev.hologramapi;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import tk.yallandev.hologramapi.controller.HologramController;
+import tk.yallandev.hologramapi.hologram.Hologram;
+import tk.yallandev.hologramapi.hologram.handler.TouchHandler;
+import tk.yallandev.hologramapi.hologram.impl.SimpleHologram;
 
 public class HologramMain extends JavaPlugin {
 	
@@ -10,11 +17,31 @@ public class HologramMain extends JavaPlugin {
 	
 	@Override
 	public void onEnable() {
-		controller = new HologramController(this);
+		controller = HologramController.createInstance(this);
+		
+		new BukkitRunnable() {
+			
+			@Override
+			public void run() {
+				Hologram hologram = controller.createHologram("caralho", new Location(Bukkit.getWorlds().get(0), 0, 120, 0), SimpleHologram.class);
+
+				hologram.setTouchHandler(new TouchHandler() {
+					
+					@Override
+					public void onTouch(Hologram hologram, Player player, TouchType touchType) {
+						player.sendMessage("eae");
+					}
+				});
+				
+				hologram.addLine("to so testando tlg");
+				hologram.addLine(controller.createHologram("otroteste", new Location(Bukkit.getWorlds().get(0), 0, 120, 0), SimpleHologram.class));
+			}
+		}.runTask(this);
 	}
 	
 	@Override
 	public void onDisable() {
+		controller.handleDisable();
 	}
 	
 }
